275ada17a2ebb1ccdd40c66f0a8ab5796b90ed4c
Bokyung Kwon <tree445560@gmail.com>
2024-09-05 14:56:25 +0900

    add: 아이디 중복 체크 기능 추가

--- src/main.js
+++ src/main.js
@@ -1,17 +1,35 @@
-// 1. 아이디 중복 체크
-
-// 2. 비밀번호 체크 기능
-
-// 3. 가입 기능
+// 가입 기능
 let idList = [];
 let submit = document.getElementById('submit__join').addEventListener('click', submitMember);
 
 function submitMember() {
   let newId = document.getElementById('member__id').value;
-  let newPw = document.getElementById('member__Pw').value;
-  let newMember = { id: newId, pw: newPw };
-  idList.push(newMember);
-  alert('가입을 완료했습니다.');
-  document.getElementById('member__id').value = '';
-  document.getElementById('member__Pw').value = '';
+  let newPw = document.getElementById('member__pw').value;
+
+  // 비밀번호 정상성 체크
+  if (newPw) {
+    alert('비밀번호를 규칙에 맞게 수정해주세요.');
+  } else {
+    // 아이디 중복 체크
+    if (idList.length <= 0) {
+      let newMember = [newId, newPw];
+      idList.push(newMember);
+      alert('가입을 완료했습니다.');
+      document.getElementById('member__id').value = '';
+      document.getElementById('member__Pw').value = '';
+    } else {
+      for (i = 0; i < idList.length; i++) {
+        if (newId == idList[i][0]) {
+          alert('중복된 아이디 입니다.');
+        } else {
+          let newMember = [newId, newPw];
+          idList.push(newMember);
+          alert('가입을 완료했습니다.');
+          document.getElementById('member__id').value = '';
+          document.getElementById('member__Pw').value = '';
+        }
+      }
+    }
+  }
+  console.log('submitMember 실행!');
 }
