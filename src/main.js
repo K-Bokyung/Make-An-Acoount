e87319819447ea3cd2095f55b119970c342d1c07
Bokyung Kwon <tree445560@gmail.com>
2024-09-06 16:01:57 +0900

    add: 비밀번호 정상성 체크 기능 추가

--- src/main.js
+++ src/main.js
@@ -5,24 +5,26 @@ let submit = document.getElementById('submit__join').addEventListener('click', s
 function submitMember() {
   let newId = document.getElementById('member__id').value;
   let newPw = document.getElementById('member__pw').value;
-  // let pattern = /[^a-z][A-Z][!@#$%*]/g;
+  let pattern = /([a-z]|[A-Z]|[0-9]|[!@#%&])/g;
+  let result = newPw.match(pattern);
 
   // 비밀번호 정상성 체크
   console.log('비밀번호 중복 체크');
-
-  // if (newPw) {
-  // alert('비밀번호를 규칙에 맞게 수정해주세요.');
-  // } else {
-
-  // 아이디 중복 체크
-  console.log('아이디 중복 체크');
-  if (idList.includes(newId)) {
-    alert('중복된 아이디 입니다.');
+  if (newPw.length > 14) {
+    alert('비밀번호가 14자를 초과했습니다.');
+  } else if (result.length !== newPw.length) {
+    alert('영문 대소문자, 숫자, 특수문자(!@#%&)로만 비밀번호를 만들어주세요.');
   } else {
-    idList.push(newId);
-    document.getElementById('member__id').value = '';
-    document.getElementById('member__pw').value = '';
-    console.log(idList);
-    alert('2.가입을 완료했습니다.');
+    // 아이디 중복 체크
+    console.log('아이디 중복 체크');
+    if (idList.includes(newId)) {
+      alert('중복된 아이디 입니다.');
+    } else {
+      idList.push(newId);
+      document.getElementById('member__id').value = '';
+      document.getElementById('member__pw').value = '';
+      console.log(idList);
+      alert('가입을 완료했습니다.');
+    }
   }
 }
